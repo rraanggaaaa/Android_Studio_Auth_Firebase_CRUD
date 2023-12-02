2739a2eb3a458cd86dca3aa22c56eb16754ee522
Desainfrontendb <147731187+Desainfrontendb@users.noreply.github.com>
2023-12-03 00:21:34 +0700

    tmr

--- app/src/main/java/com/tmr9/Login.kt
+++ app/src/main/java/com/tmr9/Login.kt
@@ -1,23 +1,24 @@
 package com.tmr9
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.widget.Button
-import android.widget.ImageButton
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tmr9.R.*
 
 class Login : ComponentActivity() {
 
-    lateinit var btnLogin : Button
-    lateinit var btnRegister : Button
+    lateinit var btnLogin: Button
+    lateinit var btnRegister: Button
+    lateinit var editTextUsername: EditText
+    lateinit var editTextPassword: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
-        installSplashScreen()
         setContentView(layout.activity_login)
 
         btnLogin = findViewById(id.btnLogin)
@@ -31,5 +32,61 @@ class Login : ComponentActivity() {
             val intentregister = Intent(this, Register::class.java)
             startActivity(intentregister)
         }
+
+        editTextUsername = findViewById(id.editTextUsername)
+        editTextPassword = findViewById(id.editTextPassword)
+
+        editTextUsername.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                // Handle Enter key press for username field
+                editTextPassword.requestFocus()
+                return@setOnKeyListener true
+            }
+            false
+        }
+
+        editTextPassword.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                // Handle Enter key press for password field
+                handleLogin()
+                return@setOnKeyListener true
+            }
+            false
+        }
+
+
+        editTextUsername = findViewById(id.editTextUsername)
+        editTextPassword = findViewById(id.editTextPassword)
+
+        editTextUsername.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                if (editTextUsername.text.isEmpty()) {
+                    showToast("Username Tidak Boleh Kosong! Mohon isi username terlebih dahulu.")
+                    editTextUsername.requestFocus()
+                } else {
+                    showToast("Username Telah Diisi!")
+                }
+            }
+        }
+
+        editTextPassword.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                if (editTextPassword.text.isEmpty()) {
+                    showToast("Password Tidak Boleh Kosong! Mohon isi password terlebih dahulu.")
+                    editTextPassword.requestFocus()
+                } else {
+                    showToast("Password Telah Diisi!")
+                }
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun handleLogin() {
+        // Implement your login logic here
+        showToast("Logging in...")
     }
 }
--- app/src/main/java/com/tmr9/MainActivity.kt
+++ app/src/main/java/com/tmr9/MainActivity.kt
@@ -11,27 +11,42 @@ class MainActivity : ComponentActivity() {
     lateinit var tv_history : ImageView
     lateinit var tv_chatus : ImageView
     lateinit var tv_gallery : ImageView
+    lateinit var tv_motor : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tv_history = findViewById(R.id.tv_history)
-        tv_history.setOnClickListener {
+        tv_history.setOnLongClickListener {
             val intentHistory = Intent(this, History::class.java)
             startActivity(intentHistory)
+
+            true
         }
 
         tv_chatus = findViewById(R.id.tv_chatus)
-        tv_chatus.setOnClickListener {
+        tv_chatus.setOnLongClickListener {
             val intentChat = Intent(this, Chat::class.java)
             startActivity(intentChat)
+
+            true
         }
 
         tv_gallery = findViewById(R.id.tv_gallery)
-        tv_gallery.setOnClickListener {
+        tv_gallery.setOnLongClickListener {
             val intentGallery = Intent(this, Gallery::class.java)
             startActivity(intentGallery)
+
+            true
+        }
+
+        tv_motor = findViewById(R.id.tv_motor)
+        tv_motor.setOnLongClickListener {
+            val intentMotor = Intent(this, Motor::class.java)
+            startActivity(intentMotor)
+
+            true
         }
     }
 }
